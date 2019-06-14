902aa63557d43ffa6362f49dd04593b5ab274374
Vincent Lomocso <vlomocso@fullscale.io>
2019-06-14 12:54:27 +0800

    - improve court tracking accuracy

--- CourtDetect2/CourtDetect2/CourtDetect2.cpp
+++ CourtDetect2/CourtDetect2/CourtDetect2.cpp
@@ -26,66 +26,16 @@ int main(int argc, char** argv)
 	// These points were manually found by impementing a click handler on the window and outputing
 	// the x,y position on the console using cout
 	vector<Point2f>	framePoints{ 
-						Point2f(454,388),
-						Point2f(769,407),
-						Point2f(1204,427),
-						Point2f(1203,438),
-						Point2f(1203,477),
-						Point2f(1201,527),
-						Point2f(1195,614),
-						Point2f(1190,651),
-						Point2f(430,541),
-						Point2f(140,463),
-						Point2f(170,455),
-						Point2f(270,431),
-						Point2f(344,413),
-						Point2f(432,393),
-
-						Point2f(388,417),
-						Point2f(306,437),
-						Point2f(497,409),
-						Point2f(297,462),
-						Point2f(638,419),
-						Point2f(436,487),
-						Point2f(720,425),
-						Point2f(524,502),
-						Point2f(812,431),
-						Point2f(638,518),
-						Point2f(1015,443),
-						Point2f(913,552),
-						Point2f(1120,473),
-						Point2f(1103,521)
+						Point2f(1,399),
+						Point2f(309,222),
+						Point2f(745,245),
+						Point2f(729,571),
 					},
 					courtPoints{
-						Point2f(29,	31),
+						Point2f(30,	320),
+						Point2f(30,31),
 						Point2f(320,31),
-						Point2f(611,31),
-						Point2f(611,47),
-						Point2f(611,147),
-						Point2f(611,221),
-						Point2f(611,321),
-						Point2f(611,337),
-						Point2f(320,337),
-						Point2f(29,	337),
-						Point2f(29,	321),
-						Point2f(29,	221),
-						Point2f(29,	147),
-						Point2f(29,	47),
-
-						Point2f(73, 147),
-						Point2f(73, 221),
-						Point2f(137,91),
-						Point2f(137,276),
-						Point2f(258,91),
-						Point2f(258,276),
-						Point2f(320,91),
-						Point2f(320,276),
-						Point2f(382,91),
-						Point2f(382,276),
-						Point2f(503,91),
-						Point2f(503,276),
-						Point2f(568,147),
-						Point2f(568,221)
+						Point2f(320,338),
 					};
 	Point2f sourcePoint, courtPoint;
 
@@ -108,32 +58,31 @@ int main(int argc, char** argv)
 	Mat resized;
 	cv::resize(undistortedFrame, resized, Size(800, 600));
 	imshow(kWinName, resized);
-	imshow("distorted", frame);
-	waitKey();
+
 	
-	//Mat homography = findHomography(framePoints, courtPoints, RANSAC);
-	//
-	//while (1) {
-	//	
-	//		//cout << "Current mouse point = " << sourcePoint.x << "," << sourcePoint.y << endl;
-	//		vector<Point2f> srcVecP{ sourcePoint };
-	//		vector<Point2f> courtVecP{ courtPoint };
-	//		perspectiveTransform(srcVecP, courtVecP, homography);
+	Mat homography = findHomography(framePoints, courtPoints, RANSAC);
+	
+	while (1) {
+		
+			//cout << "Current mouse point = " << sourcePoint.x << "," << sourcePoint.y << endl;
+			vector<Point2f> srcVecP{ sourcePoint };
+			vector<Point2f> courtVecP{ courtPoint };
+			perspectiveTransform(srcVecP, courtVecP, homography);
 
-	//		courtPoint = courtVecP[0];
+			courtPoint = courtVecP[0];
 
-	//		Mat courtClone = court.clone();
-	//		circle(courtClone, courtPoint, 3, Scalar(255), 1, LINE_8);
+			Mat courtClone = court.clone();
+			circle(courtClone, courtPoint, 3, Scalar(255), 1, LINE_8);
 
-	//	imshow(kWinName, frame);
-	//	imshow("Court", courtClone);
+		imshow(kWinName, resized);
+		imshow("Court", courtClone);
 
-	//	char key = (char)waitKey(30);
-	//	if (key == 'q' || key == 27)
-	//	{
-	//		break;
-	//	}
-	// }
+		char key = (char)waitKey(30);
+		if (key == 'q' || key == 27)
+		{
+			break;
+		}
+	 }
 	return 0;
 }
 
